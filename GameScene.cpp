3e214112fe2b4e7fe6e4cb2f10bd77176974b6bf
YutoArake <k021g1420@g.neec.ac.jp>
2022-10-07 15:08:50 +0900

    .objファイル読み込み

--- GameScene.cpp
+++ GameScene.cpp
@@ -133,8 +133,8 @@ void GameScene::Draw()
 	/// </summary>
 
 	// 描画
-	sprite1->Draw();
-	sprite2->Draw();
+	// sprite1->Draw();
+	// sprite2->Draw();
 
 	// デバッグテキストの描画
 	debugText.DrawAll(cmdList);
